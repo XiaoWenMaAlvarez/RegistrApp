1253522e982aca5d1caf0420c745cbac2518a5ed
Xiao Ma <xi.ma@duocuc.cl>
2024-11-12 23:30:57 -0300

    Se realizan las pruebas unitarias

--- src/app/services/alumnos/alumnos.service.spec.ts
+++ src/app/services/alumnos/alumnos.service.spec.ts
@@ -2,15 +2,43 @@ import { TestBed } from '@angular/core/testing';
 
 import { AlumnosService } from './alumnos.service';
 
+import {AngularFireModule} from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import { firstValueFrom } from 'rxjs';
+import { Alumno } from 'src/app/models/alumno';
+
 describe('AlumnosService', () => {
   let service: AlumnosService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [AngularFireModule.initializeApp(environment.firebaseConfig)]
+    });
     service = TestBed.inject(AlumnosService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('Encontrar alumno de prueba', async () => {
+    let sub$ = service.encontrarAlumnoPorId("82vAPSM8toZvnrt2Nxd7jWstquG3");
+    const alumno: Alumno[] = await firstValueFrom(sub$) as Alumno[];
+    let nombre: string = alumno[0].nombre;
+    expect(nombre).toEqual("Juan Alberto Pérez Gonzales");
+  });
+
+  it('Encontrar alumno por correo', async () => {
+    let sub$ = service.encontrarAlumnoPorCorreo("ju.perez@duocuc.cl");
+    const alumno: Alumno[] = await firstValueFrom(sub$) as Alumno[];
+    let nombre: string = alumno[0].nombre;
+    expect(nombre).toEqual("Juan Alberto Pérez Gonzales");
+  });
+
+  it('Login con alumno', async () => {
+    let res = await service.login("ju.perez@duocuc.cl", "alumno123");
+    expect(res.user.uid).toEqual("82vAPSM8toZvnrt2Nxd7jWstquG3");
+    service.logout();
+  });
+
 });
--- src/app/services/profesor/profesor.service.spec.ts
+++ src/app/services/profesor/profesor.service.spec.ts
@@ -1,16 +1,44 @@
+import { Profesor } from 'src/app/models/profesor';
 import { TestBed } from '@angular/core/testing';
 
 import { ProfesorService } from './profesor.service';
+
+import {AngularFireModule} from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import { firstValueFrom } from 'rxjs';
 
 describe('ProfesorService', () => {
   let service: ProfesorService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [AngularFireModule.initializeApp(environment.firebaseConfig)]
+    });
     service = TestBed.inject(ProfesorService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('Encontrar profesor de prueba', async () => {
+    let sub$ = service.encontrarProfesorPorId("NeIKnsD1IhREsIjCysdtRyBaPDS2");
+    const profesor: Profesor[] = await firstValueFrom(sub$) as Profesor[];
+    let nombre: string = profesor[0].nombre;
+    expect(nombre).toEqual("Erik Andrés Arcos Rojas");
+  });
+
+  it('Encontrar profesor por correo', async () => {
+    let sub$ = service.encontrarProfesorPorCorreo("er.arcos@profesor.duoc.cl");
+    const profesor: Profesor[] = await firstValueFrom(sub$) as Profesor[];
+    let nombre: string = profesor[0].nombre;
+    expect(nombre).toEqual("Erik Andrés Arcos Rojas");
+  });
+
+  it('Login con profesor', async () => {
+    let res = await service.login("er.arcos@profesor.duoc.cl", "profesor123");
+    expect(res.user.uid).toEqual("NeIKnsD1IhREsIjCysdtRyBaPDS2");
+    service.logout();
+  });
+
 });
